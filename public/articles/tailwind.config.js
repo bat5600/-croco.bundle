325bdd58d80a279a0109dc7e7da194ed2557d3cc
bat5600 <baptiste@alligator-ads.fr>
2025-12-27 19:09:42 +0100

    Revert "Adjust mobile prose typography size"

--- public/articles/tailwind.config.js
+++ public/articles/tailwind.config.js
@@ -23,13 +23,6 @@ export default {
           900: "#064e3b"
         }
       }
-    },
-    typography: {
-      sm: {
-        css: {
-          fontSize: "0.8rem"
-        }
-      }
     }
   },
   plugins: [require("@tailwindcss/typography")]
