67d7e9273cfc3e0c28c4e9c60dd5288e3a39d88e
bat5600 <baptiste@alligator-ads.fr>
2025-12-27 16:53:02 +0100

    Delete public/articles/build-tailwind.js

--- public/articles/build-tailwind.js
+++
@@ -1,6 +0,0 @@
-import { execSync } from "node:child_process";
-
-execSync(
-  "npx tailwindcss -c articles/tailwind.config.js -i articles/input.css -o articles/croco-article.css --minify",
-  { stdio: "inherit" }
-);
